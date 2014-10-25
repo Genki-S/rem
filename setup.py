33dda2a52f9e345c222e1251f577040b681dd2fe
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-10-25 21:33:43 +0900

    Write setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='rem',
+    description='Set reminders blazing fast.',
+    version='1.0.0',
+    install_requires=[
+        'parsedatetime'
+    ],
+    author='Genki Sugimoto',
+    author_email='cfhoyuk.reccos.nelg@gmail.com',
+    url='http://github.com/Genki-S/rem/',
+    download_url='https://pypi.python.org/pypi/rem/',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'rem=rem:main',
+        ],
+    },
+)
